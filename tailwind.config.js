b6c777ef1a69e5b5cc8335d781638a6257fde1d0
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-05-13 20:01:38 -0300

    colors palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,24 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+      colors: {
+        primary: '#fffffe',
+        secondary: '#f25f4c',
+        tertiary: '#e53170',
+        highlight: '#ff8906',
+        black: '#000000',
+        white: '#ffffff',
+        background: '#0f0e17',
+        headline: '#fffffe',
+        paragraph: '#a7a9be',
+        button: '#ff8906',
+        buttonText: '#fffffe'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
