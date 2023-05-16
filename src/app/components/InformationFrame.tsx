ff7f6a3085f4cb939805e595039775f49555dabe
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-05-15 22:57:27 -0300

    InformationFrame component

---
+++ src/app/components/InformationFrame.tsx
@@ -0,0 +1,23 @@
+import { IconType } from 'react-icons/lib'
+
+interface InformationFrameProps {
+  title: string
+  description: string
+  icon: IconType
+}
+
+const InformationFrame = ({
+  title,
+  description,
+  icon
+}: InformationFrameProps) => {
+  return (
+    <div className="flex flex-col items-start justify-start gap-4 bg-background p-10 shadow-md mx-8">
+      {icon({ className: 'text-5xl text-primary' })}
+      <h3 className="text-2xl text-headline">{title}</h3>
+      <p className="text-paragraph leading-8">{description}</p>
+    </div>
+  )
+}
+
+export default InformationFrame
