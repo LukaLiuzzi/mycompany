517805353de7b29056118f3fdfc8be2fdb9866d8
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-05-17 02:45:34 -0300

    change div to section

--- src/app/components/Carrousell.tsx
+++ src/app/components/Carrousell.tsx
@@ -9,14 +9,14 @@ interface CarrousellProps {
 
 const Carrousell = ({ title, components }: CarrousellProps) => {
   return (
-    <div className="bg-primary p-8 h-[200px]">
+    <section className="bg-primary p-8 h-[200px]">
       <h3 className="text-2xl text-center text-bg p-10 font-bold">{title}</h3>
       <Slider {...carrousellSettings}>
         {components.map((component, index) => (
           <div key={index}>{component}</div>
         ))}
       </Slider>
-    </div>
+    </section>
   )
 }
 
