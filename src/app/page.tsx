6bbd5bc4648161e0bc09bce2ad66e89834f87889
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-05-17 02:45:22 -0300

    ContactSection and Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,45 @@
 import AboutSection from './components/AboutSection'
 import Carrousell from './components/Carrousell'
+import ContactSection from './components/ContactSection'
 import Hero from './components/Hero'
 import InformationFrame from './components/InformationFrame'
+import Navbar from './components/Navbar'
 import { professionalServices } from './constants'
 
 export default function Home() {
   return (
-    <main>
-      <Hero
-        image="/images/hero.jpg"
-        imageAlt="Hero background"
-        title="Desarrollo de software"
-        subTitle="Especialistas en"
-        buttonText="Conocer mas"
-        buttonHref="/about"
-        titlePosition="center"
-      />
-      <AboutSection
-        title="Red de Consultoría"
-        description="Ofreciendo un servicio diferencial, nos hemos posicionado en el mercado argentino como referente en consultoría de negocios."
-        AnotherDescription="Nuestra red de Consultoría, esta conformado por profesionales especializados en el diseño, desarrollo y obtención de nuevas oportunidades de negocios para nuestros clientes."
-        buttonHref="/about"
-        buttonText="Ver mas"
-      />
-      <Carrousell
-        title="Servicios Profesionales"
-        components={professionalServices.map((service) => (
-          <InformationFrame
-            key={service.id}
-            title={service.title}
-            description={service.description}
-            icon={service.icon}
-          />
-        ))}
-      />
-    </main>
+    <>
+      <Navbar />
+      <main>
+        <Hero
+          image="/images/hero.jpg"
+          imageAlt="Hero background"
+          title="Desarrollo de software"
+          subTitle="Especialistas en"
+          buttonText="Conocer mas"
+          buttonHref="/about"
+          titlePosition="center"
+        />
+        <AboutSection
+          title="Red de Consultoría"
+          description="Ofreciendo un servicio diferencial, nos hemos posicionado en el mercado argentino como referente en consultoría de negocios."
+          AnotherDescription="Nuestra red de Consultoría, esta conformado por profesionales especializados en el diseño, desarrollo y obtención de nuevas oportunidades de negocios para nuestros clientes."
+          buttonHref="/about"
+          buttonText="Ver mas"
+        />
+        <Carrousell
+          title="Servicios Profesionales"
+          components={professionalServices.map((service) => (
+            <InformationFrame
+              key={service.id}
+              title={service.title}
+              description={service.description}
+              icon={service.icon}
+            />
+          ))}
+        />
+        <ContactSection />
+      </main>
+    </>
   )
 }
