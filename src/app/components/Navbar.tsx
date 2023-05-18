0ba8932956201cd63431b74b739de54ee4d9bbe6
Luka Liuzzi <lukaliuzzidev@gmail.com>
2023-05-17 21:21:39 -0300

    Navbar fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -20,22 +20,22 @@ const Navbar = () => {
     <>
       <header className="bg-transparent absolute top-0 left-0 w-full text-white shadow-lg md:block">
         <div className="lg:container lg:mx-auto lg:flex lg:items-center">
-          <Link
-            href="/"
-            className="flex lg:items-center lg:justify-center p-4 z-40"
-          >
-            <Image
-              src="https://i.ibb.co/6Yxs70d/2021-10-26-23h27-03.png"
-              alt="Logo"
-              height={50}
-              width={50}
-            />
-            <span className="ml-4 uppercase font-black text-center flex justify-center items-center">
-              Luka
-              <br />
-              Liuzzi
-            </span>
-          </Link>
+          <div className="flex relative lg:items-center lg:justify-center p-4 z-40">
+            <Link href="/" className="flex items-center justify-center">
+              <Image
+                src="https://i.ibb.co/6Yxs70d/2021-10-26-23h27-03.png"
+                alt="Logo"
+                height={50}
+                width={50}
+              />
+              <span className="ml-4 uppercase font-black text-center flex justify-center items-center">
+                Luka
+                <br />
+                Liuzzi
+              </span>
+            </Link>
+          </div>
+
           <nav
             className={`font-semibold text-base lg:text-lg lg:flex-1 lg:flex lg:justify-center ${
               isMenuOpen ? 'flex' : 'hidden'
